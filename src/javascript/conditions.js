af7c12d782063481e07e519c5d6c7ce07ef7fb8a
procharlie8 <charlienewman2507@gmail.com>
2023-11-30 18:58:52 +0000

    else if conditions

--- src/javascript/conditions.js
+++ src/javascript/conditions.js
@@ -3,8 +3,14 @@ if (charlie) {
   console.log("hello charlie");
 }
 
-var me = 10;
-var isMeGreaterThan5 = me > 5;
-if (isMeGreaterThan5) {
-  console.log("me is greater than 5");
+var number = 0;
+var isNumberGreaterThan5 = number > 5;
+if (isNumberGreaterThan5) {
+  console.log("number is greater than 5");
+} else if (number > 0) {
+  console.log("number is less than 5 and more than 0");
+} else {
+  console.log("number is 0 or less than 0");
 }
+
+console.log("The value of number is:", number);
